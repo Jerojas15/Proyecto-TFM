e82475824e3ccad3c1ea6a75e195c6ca3899e9dd
Laura <lalitapenagos98@hotmail.com>
2021-04-06 02:24:47 +0000

    Creacion de prototipo de la villa, bosque, animacion personaje,objetos

---
+++ Assets/Script/bomba.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomba : MonoBehaviour
+{
+
+    public GameObject objeto1;
+    public GameObject objeto2;
+    public GameObject objeto3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("rocaGigante"))
+        {
+            Destroy(objeto1);
+            Destroy(objeto2);
+            Destroy(objeto3);
+
+        }
+    }
+
+    
+}
+
---
+++ Assets/Script/boton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boton : MonoBehaviour
+{
+
+    public GameObject objeto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.CompareTag("Player"))
+        {
+            Destroy(objeto);
+        }
+    }
+}
+
+ 
