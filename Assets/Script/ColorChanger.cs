75f04536a31e368d434aa012a0146a5d4ca5460a
Julio <julioesteban2@hotmail.com>
2021-04-07 00:40:03 +0000

    Filtro de color v1 agregado

---
+++ Assets/Script/ColorChanger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ColorChanger : MonoBehaviour {
+    ColorGrading colorGrading;
+    private bool isActive = false;
+
+    [SerializeField]
+    private GameObject farestPoint;
+
+    [SerializeField]
+    private GameObject startPoint;
+
+    [SerializeField]
+    private GameObject player;
+
+    void Start() {
+        GetComponent<PostProcessVolume>().profile.TryGetSettings(out colorGrading);
+    }
+
+    public void setColorFilter() {
+        isActive = !isActive;
+        colorGrading.active = isActive;
+
+        if (colorGrading && isActive) {
+            colorGrading.enabled.value = true;
+            colorGrading.colorFilter.value = new Color(0.584f, 0.559f, 1.774f, 1);
+            colorGrading.lift.value = new Vector4(0, 0, 1, 0);
+            colorGrading.gamma.value = new Vector4(0, 0, 1, 0);
+            colorGrading.gain.value = new Vector4(0, 0, 1, 0);
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            setColorFilter();
+        }
+        float playerDistance = Vector3.Distance(player.transform.position, startPoint.transform.position);
+        float pointsDistance = Vector3.Distance(farestPoint.transform.position, startPoint.transform.position);
+
+        if (!isActive) {
+            if (playerDistance < pointsDistance) {
+                colorGrading.active = true;
+                float percentage = 1 - (playerDistance / pointsDistance);
+                colorGrading.enabled.value = true;
+                colorGrading.colorFilter.value = new Color(0.584f, 0.559f, 1.774f, 1);
+                colorGrading.lift.value = new Vector4(1 * percentage, 0, 0, 0);
+                colorGrading.gamma.value = new Vector4(1 * percentage, 0, 0, 0);
+                colorGrading.gain.value = new Vector4(1 * percentage, 0, 0, 0);
+            } else {
+                colorGrading.active = false;
+            }
+        }
+    }
+}
---
+++ Assets/Script/enemies/FlieEnemy.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class FlieEnemy : MonoBehaviour
+{
+    enum State { moving, targeting, attacking, dying };
+    enum Rotation { down, up };
+
+    [SerializeField] private float timeMoving;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float topAngle;
+    [SerializeField] private float bottomAngle;
+    [SerializeField] private GameObject raycastLauncher;
+
+    private float startMovingTime = 0;
+    private Rotation direction = Rotation.down;
+    private State actualState = State.moving;
+    private Vector3 targetPosition;
+
+    void Start() {
+    }
+
+    void Update()
+    {
+        switch (actualState) {
+            case State.moving:
+                move();
+                break;
+            case State.targeting:
+                searchTarget();
+                break;
+            case State.attacking:
+                attack();
+                break;
+            case State.dying:
+                die();
+                break;
+        }
+    }
+
+    private void move() {
+        if (startMovingTime + Time.time < startMovingTime + timeMoving) {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        } else {
+            actualState = State.targeting;
+        }
+    }
+
+    private void searchTarget() {
+        if (transform.rotation.eulerAngles.z > bottomAngle) {
+            if (direction == Rotation.down) {
+                direction = Rotation.up;
+            } else if (direction == Rotation.up) {
+                direction = Rotation.down;
+            }
+        }
+
+        var rotationDirection = 1;
+        if (direction == Rotation.up) {
+            rotationDirection = -1;
+        }
+        
+        transform.Rotate(rotationDirection * Vector3.forward, rotationSpeed * Time.deltaTime);
+        Debug.DrawRay(raycastLauncher.transform.position, -transform.right * 20f, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(raycastLauncher.transform.position, -transform.right, 20f);
+
+        if (hit.collider != null && hit.collider.transform.CompareTag("Player")) {
+            actualState = State.attacking;
+            targetPosition = hit.collider.transform.position;
+        }
+    }
+
+    private void attack() {
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, attackSpeed * Time.deltaTime);
+    }
+
+    private void die() {
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (actualState == State.attacking) {
+            actualState = State.dying;
+        }
+    }
+}
