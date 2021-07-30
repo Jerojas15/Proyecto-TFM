3294261b0feb06941c170cea5a1f7e663333cf08
Patrick Harvey <normanpatrickha@gmail.com>
2021-07-30 00:19:14 +0000

    Cambios

---
+++ Assets/Patrick/PuntoControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuntoControl : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            player.transform.position = new Vector3(761.8489f, 8.66412f, 0);
+        }
+    }
+}
